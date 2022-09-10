4ce29a67ed0f39d87dfd94430d22265f895d6053
Alley <alleychaggar@gmail.com>
2022-09-09 22:27:42 -0400

    Script Added

--- Assets/[Scripts]/PlayerBehaviour.cs
+++ Assets/[Scripts]/PlayerBehaviour.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerBehaviour : MonoBehaviour
 {
+    public float speed = 2;
+    public SpriteRenderer renderer;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,22 @@ public class PlayerBehaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
+    }
+
+    public void Move()
+    {
+        float x = Input.GetAxisRaw("Horizontal") * Time.deltaTime * speed;
+
+        switch (x)
+        {
+            case < 0:
+                renderer.flipX = true;
+                break;
+            case > 0:
+                renderer.flipX = false;
+                break;
+        }
+        transform.position += new Vector3(x, 0);
     }
 }
